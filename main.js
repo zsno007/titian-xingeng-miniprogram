62e11949b82ad39b2a80a82b621dc31ba9b28da2
zsno007 <zsable007@outlook.com>
2025-06-06 18:16:31 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
 import App from './App'
+import i18n from './lang/i18n'
+
 
 // #ifndef VUE3
 import Vue from 'vue'
-import './uni.promisify.adaptor'
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
-  ...App
+	i18n,
+	...App
 })
 app.$mount()
 // #endif
 
+
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {createSSRApp} from 'vue'
+
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	app.use(i18n)
+	return {app}
 }
 // #endif
